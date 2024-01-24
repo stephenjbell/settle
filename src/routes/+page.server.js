cff51b4926538139f2e0caf3d754e47e5d189231
Stephen Bell <stephen@steedokc.com>
2024-01-24 17:03:39 -0600

    Make timestamp use CDT

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -2,7 +2,7 @@ export const prerender = true;
 
 /** @type {import('./$types').PageServerLoad} */
 export async function load() {
-	const date = new Date().toLocaleString();
+	const date = new Date().toLocaleString('en-US', { timeZone: 'America/Chicago' });
 
 	return {
 		date: date
