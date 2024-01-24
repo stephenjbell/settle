43ef6934238214174f197727c89e932faf06c3af
Stephen Bell <stephen@steedokc.com>
2024-01-24 12:19:23 -0600

    Remove date for now

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,6 +1,5 @@
 /** @type {import('./$types').PageServerLoad} */
 export async function load() {
-	// Get current date and time in human readable format
 	const date = new Date().toLocaleString();
 
 	return {
